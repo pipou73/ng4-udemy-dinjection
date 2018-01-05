4d759d8c4e2ae64d028275851444f8e08582322b
vesin philippe <pvesin@sapiens.biz>
2018-01-05 17:34:37 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    },
+  ]
+
+  onAccountAdded(newAccount: {name: string; status: string}) {
+    this.accounts.push(newAccount);
+  }
+
+  onStatusChanged(updateinfo: {id: number, newStatus: string}) {
+    this.accounts[updateinfo.id].status = updateinfo.newStatus
+  }
+}
